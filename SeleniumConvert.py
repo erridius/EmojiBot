4bdd290b2274b8e00a631f992d5f1dc51af8bee1
kozenkodev <65870072+kozenkodev@users.noreply.github.com>
2020-05-26 02:30:58 +0300

    Fixed error with empty message and change module
     - Add function for notification user about can't translate message;
     - Rewrite function for work with selenium on sync module.

--- SeleniumConvert.py
+++ SeleniumConvert.py
@@ -15,33 +15,42 @@ emotio=Emoticons()
 bot = Bot(token=Config.telegram_token)
 dp = Dispatcher(bot)
 class SeleniumComplete(object):
-
+     driver = webdriver.Firefox()
      #Translate from emoji to auto-detect user language
      async def StartTask_fromemo(self, text, message):
-         async with get_session(Geckodriver(), Firefox(firefoxOptions={ 'args': ['-headless'] })) as session:
+         try:
 
-            await session.get(f'https://translate.yandex.ru/?lang=emj-{str(message.from_user.language_code)}')
-            original = await session.wait_for_element(5, 'textarea')
-            await original.send_keys(text)
-            translate = await session.wait_for_element(6, 'pre#translation')
-            done_translate=await translate.get_text()
-            print(done_translate)
-            if emotio.is_emoji(done_translate)==False and len(done_translate)==0:
-             time.sleep(6)
-             done_translate = await translate.get_text()
-            await message.answer(done_translate)
+             self.driver.get(f'https://translate.yandex.ru/?lang=emj-{str(message.from_user.language_code)}')
+             item= self.driver.find_element_by_css_selector('textarea')
+             item.send_keys(text)
+             time.sleep(7)
+             item = self.driver.find_element_by_xpath("//pre[contains(@class,'textinput textlayer translation')]")
+             translate=item.text
+
+             if emotio.is_emoji(translate)==False and len(translate)==0:
+                 time.sleep(6)
+                 translate =  item.text
+             await message.answer(translate)
+         except Exception as e:
+             print(e)
+             await message.answer("Извини,но я не смог перевести....Попробуй еще раз или другой текст")
+
 
      #Translate from user auto-detect language to emoji
      async def StartTask_toemo(self,text,message):
-         async with get_session(Geckodriver(), Firefox(firefoxOptions={ 'args': ['-headless'] })) as session:
+         try:
 
-            await session.get('https://translate.yandex.ru/?lang=en-emj')
-            original = await session.wait_for_element(4, 'textarea')
-            await original.send_keys(text)
-            translate = await session.wait_for_element(4, 'pre#translation')
-            done_translate=await translate.get_text()
-            print(done_translate)
-            if emotio.is_emoji(done_translate)==False and len(done_translate)==0:
-             time.sleep(6)
-             done_translate = await translate.get_text()
-            await message.answer(done_translate)
+             self.driver.get(f'https://translate.yandex.ru/?lang=en-emj')
+             item = self.driver.find_element_by_css_selector('textarea')
+             item.send_keys(text)
+             time.sleep(8)
+             item = self. driver.find_element_by_xpath("//pre[contains(@class,'textinput textlayer translation')]")
+             translate = item.text
+
+             if emotio.is_emoji(translate) == False and len(translate) == 0:
+                     time.sleep(6)
+                     translate =  item.text
+             await message.answer(translate)
+         except Exception as e:
+             print(e)
+             await message.answer("Извини,но я не смог перевести....Попробуй еще раз или другой текст")
